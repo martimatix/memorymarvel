81e264e39b12235d7d22a217710cac931adf42c2
Mario Martinez <zenitram.oiram@gmail.com>
2015-03-28 12:29:51 +1100

    search view created

--- app/assets/javascripts/router/router.js
+++ app/assets/javascripts/router/router.js
@@ -4,12 +4,19 @@ var app = app || {};
 // Makes the application navigatable, with meaningful URLs.
 app.AppRouter = Backbone.Router.extend({
   routes: {
-    '' : 'index'
+    '' : 'index',
+    'deck/:id/search' : 'searchForComics'
   },
 
   index: function () {
     var deckView = new app.DeckView({collection: app.decks});
     deckView.render();
+  },
+
+  searchForComics: function (id) {
+    var deck = app.decks.get(id);
+    var searchView = new app.SearchView({model: deck});
+    searchView.render();
   }
 
 });
---
+++ app/assets/javascripts/views/searchView.js
@@ -0,0 +1,13 @@
+var app = app || {};
+
+// Like Rails views but with the event handling stored here as well.
+// Responsible for showing data on the page, but also allowing interaction.
+app.SearchView = Backbone.View.extend({
+  el: '#main', // define the selector which this view is associated with
+  render: function () {
+
+    var searchViewHTML = $('#searchView-template').html();
+    this.$el.html(searchViewHTML);
+
+  }
+});
