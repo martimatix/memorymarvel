342f1ac68371e041103026751105ae3e8a77602c
Mario Martinez <zenitram.oiram@gmail.com>
2015-03-28 17:46:31 +1100

    search is working

--- app/assets/javascripts/views/searchView.js
+++ app/assets/javascripts/views/searchView.js
@@ -4,10 +4,34 @@ var app = app || {};
 // Responsible for showing data on the page, but also allowing interaction.
 app.SearchView = Backbone.View.extend({
   el: '#main', // define the selector which this view is associated with
+  events: {
+    'click #search': 'renderImages'
+  },
   render: function (page) {
-
     var searchViewHTML = $('#searchView-template').html();
     this.$el.html(searchViewHTML);
+  },
 
+  renderImages: function (event) {
+    self = this;
+    this.searchMarvel().then(function (result){
+      for (var i = 0; i < result.data.results.length; i++) {
+        var image_path = result.data.results[i].thumbnail.path;
+        self.$el.append('<img src="' + image_path + '/portrait_xlarge.jpg">');
+      };
+    });
+  },
+
+  searchMarvel: function () {
+    event.preventDefault();
+    var marvelUrl = 'http://gateway.marvel.com:80/v1/public/comics?';
+
+    return $.getJSON(marvelUrl, {
+      format: 'comic',
+      formatType: 'comic',
+      noVariants: 'true',
+      titleStartsWith: $('#comic_title').val(),
+      apikey: 'e05840ef82f3caa5b3e1483b2a7b9d11'
+    });
   }
 });
