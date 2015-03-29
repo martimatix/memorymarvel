ddd9ab786ad8e32c2a0a4d72cdc520834b0b61cd
Mario Martinez <zenitram.oiram@gmail.com>
2015-03-29 11:48:54 +1100

    next page of search results  working in a clunky way

--- app/assets/javascripts/views/searchView.js
+++ app/assets/javascripts/views/searchView.js
@@ -1,19 +1,19 @@
 var app = app || {};
 
-// Like Rails views but with the event handling stored here as well.
-// Responsible for showing data on the page, but also allowing interaction.
 app.SearchView = Backbone.View.extend({
   el: '#main', // define the selector which this view is associated with
+  page_number: 0, // page number for search results
   events: {
     'click #search': 'renderImages',
-    'click .comic-cover': 'addComicToDeck'
+    'click .comic-cover': 'addComicToDeck',
+    'click .turn-page': 'nextOrPreviousResults'
   },
   render: function (page) {
     var searchViewHTML = $('#searchView-template').html();
     this.$el.html(searchViewHTML);
   },
 
-  renderImages: function (event) {
+  renderImages: function () {
     self = this;
     this.searchMarvel().then(function (result){
       for (var i = 0; i < result.data.results.length; i++) {
@@ -21,12 +21,12 @@ app.SearchView = Backbone.View.extend({
         self.$el.append('<img src="' + image_path + '/portrait_xlarge.jpg" data-counter="' + i + '" class="comic-cover">');
       };
       self.responseJSON = result.data.results;
+      self.$el.append('<p class="turn-page" id="next">Next</p>')
     });
   },
 
   searchMarvel: function () {
     console.log('searching marvel');
-    event.preventDefault();
     var marvelUrl = 'http://gateway.marvel.com:80/v1/public/comics?';
 
     return $.getJSON(marvelUrl, {
@@ -34,12 +34,13 @@ app.SearchView = Backbone.View.extend({
       formatType: 'comic',
       noVariants: 'true',
       titleStartsWith: $('#comic_title').val(),
+      offset: this.page_number * 20,
       apikey: 'e05840ef82f3caa5b3e1483b2a7b9d11'
     });
   },
 
   addComicToDeck: function(event) {
-    var counter = $(event.target).attr('data-counter') // counter is the position in this.responseJSON
+    var counter = $(event.target).attr('data-counter'); // counter is the position in this.responseJSON
     var comicInfoObject = this.responseJSON[counter];
     var newComic = new app.Comic({
       marvel_id: comicInfoObject.id,
@@ -48,5 +49,17 @@ app.SearchView = Backbone.View.extend({
       deck_id: this.model.get('id')
     });
     newComic.save();
+  },
+
+  nextOrPreviousResults: function(event) {
+    event.preventDefault();
+    var nextOrPrevious = $(event.target).attr('id');
+    if (nextOrPrevious === 'next') {
+      this.page_number += 1;
+      this.renderImages();
+    } else {
+      this.page_number -= 1;
+      this.renderImages();
+    }
   }
 });
