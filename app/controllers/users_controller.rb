1d19178209a72f08118dc2fe09d14ba09eecaa49
Mario Martinez <zenitram.oiram@gmail.com>
2015-03-29 17:29:09 +1100

    user redirected to home page when they sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,16 +24,12 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user = User.new user_params
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
     end
   end
 
